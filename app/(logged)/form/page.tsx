172080b1727f0a09935b95be983a570a454d2ca9
andresdossantos <andres.eds00@gmail.com>
2025-02-18 10:17:10 +0100

    Inicio de diseño, botones, nav y primera fase del formulario de usuario /form

--- app/(logged)/form/page.tsx
+++ app/(logged)/form/page.tsx
@@ -29,7 +29,7 @@ const Form = () => {
                       </a>
                   </li>
                   <li>
-                      <a href="aaa" className="hover:underline">
+                      <a href="aaaa" className="hover:underline">
                           Gráficas
                       </a>
                   </li>
