a8407fab76328f639fbdaa2470d28f9448ce4437
andresdossantos <andres.eds00@gmail.com>
2025-02-19 23:59:17 +0100

    Nuevo formulario y modificación de la tabla FORMS en el django

--- api/baseAppBackendAPI.schemas.ts
+++ api/baseAppBackendAPI.schemas.ts
@@ -7,6 +7,22 @@
  * API para el sistema de BaseApp Backend
  */
 
+/**
+ * * `Agricultura / Pesca` - Agricultura Pesca
+ * `Tecnología / Innovación` - Tecnologia Innovacion
+ * `Negocios / Emprendimiento` - Negocios Emprendimiento
+ * `Educación / Salud` - Educacion Salud
+ * `Industria / Manufactura` - Industria Manufactura
+ * `Otros` - Otros
+ */
+export enum WorkSectorEnum {
+  "Agricultura_/_Pesca" = "Agricultura / Pesca",
+  "Tecnología_/_Innovación" = "Tecnología / Innovación",
+  "Negocios_/_Emprendimiento" = "Negocios / Emprendimiento",
+  "Educación_/_Salud" = "Educación / Salud",
+  "Industria_/_Manufactura" = "Industria / Manufactura",
+  Otros = "Otros",
+}
 export interface UserRegistration {
   /**
    * Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.
@@ -31,11 +47,43 @@ export interface User {
   email?: string;
 }
 
+/**
+ * * `Sí, quiero un país con apoyo social.` - Si
+ * `No me importa.` - No
+ */
+export enum UnemploymentSupportEnum {
+  "Sí,_quiero_un_país_con_apoyo_social" = "Sí, quiero un país con apoyo social.",
+  No_me_importa = "No me importa.",
+}
+/**
+ * * `Quiero un país con muy baja tasa de desempleo.` - Baja Tasa
+ * `No me importa mucho la tasa de desempleo.` - No Importa
+ */
+export enum UnemploymentRateEnum {
+  Quiero_un_país_con_muy_baja_tasa_de_desempleo = "Quiero un país con muy baja tasa de desempleo.",
+  No_me_importa_mucho_la_tasa_de_desempleo = "No me importa mucho la tasa de desempleo.",
+}
 export interface TokenRefresh {
   readonly access: string;
   refresh: string;
 }
 
+/**
+ * * `Sí, prefiero un país innovador.` - Si
+ * `No es una prioridad para mí.` - No
+ */
+export enum ResearchDevelopmentEnum {
+  "Sí,_prefiero_un_país_innovador" = "Sí, prefiero un país innovador.",
+  No_es_una_prioridad_para_mí = "No es una prioridad para mí.",
+}
+/**
+ * * `Muy importante, quiero un país con menos desigualdad.` - Muy Importante
+ * `No es una prioridad.` - No Importa
+ */
+export enum PovertyReductionEnum {
+  "Muy_importante,_quiero_un_país_con_menos_desigualdad" = "Muy importante, quiero un país con menos desigualdad.",
+  No_es_una_prioridad = "No es una prioridad.",
+}
 export interface PatchedUser {
   readonly id?: number;
   /**
@@ -48,7 +96,163 @@ export interface PatchedUser {
   email?: string;
 }
 
+/**
+ * * `Muy importante.` - Muy Importante
+ * `No me importa.` - No Importa
+ */
+export enum MobileAccessEnum {
+  Muy_importante = "Muy importante.",
+  No_me_importa = "No me importa.",
+}
+/**
+ * * `Sí, quiero un país con menor carga impositiva.` - Si
+ * `No me importa.` - No
+ */
+export enum LowTaxesEnum {
+  "Sí,_quiero_un_país_con_menor_carga_impositiva" = "Sí, quiero un país con menor carga impositiva.",
+  No_me_importa = "No me importa.",
+}
+/**
+ * * `Ciudad` - Ciudad
+ * `Rural` - Rural
+ * `No me importa` - No Importa
+ */
+export enum LivingAreaEnum {
+  Ciudad = "Ciudad",
+  Rural = "Rural",
+  No_me_importa = "No me importa",
+}
+/**
+ * * `Prefiero un país con empleos estables y buenos contratos.` - Empleos Estables
+ * `No me importa si el empleo es informal o vulnerable.` - Informal
+ */
+export enum JobSecurityEnum {
+  Prefiero_un_país_con_empleos_estables_y_buenos_contratos = "Prefiero un país con empleos estables y buenos contratos.",
+  No_me_importa_si_el_empleo_es_informal_o_vulnerable = "No me importa si el empleo es informal o vulnerable.",
+}
+/**
+ * * `Muy importante, quiero un país con buena atención médica.` - Muy Importante
+ * `No me importa mucho.` - No Importa
+ */
+export enum HealthcareImportanceEnum {
+  "Muy_importante,_quiero_un_país_con_buena_atención_médica" = "Muy importante, quiero un país con buena atención médica.",
+  No_me_importa_mucho = "No me importa mucho.",
+}
+/**
+ * * `Hombre` - Male
+ * `Mujer` - Female
+ */
+export enum GenderEnum {
+  Hombre = "Hombre",
+  Mujer = "Mujer",
+}
+/**
+ * * `Sí, quiero un país con un alto nivel de comercio internacional.` - Si
+ * `No es relevante para mí.` - No Importa
+ */
+export enum ExportEconomyEnum {
+  "Sí,_quiero_un_país_con_un_alto_nivel_de_comercio_internacional" = "Sí, quiero un país con un alto nivel de comercio internacional.",
+  No_es_relevante_para_mí = "No es relevante para mí.",
+}
 export interface EmailTokenObtain {
   email: string;
   password: string;
+}
+
+/**
+ * * `Muy importante (Quiero emprender o invertir).` - Muy Importante
+ * `Moderadamente importante (Podría considerar oportunidades).` - Moderado
+ * `No es importante para mí.` - No Importante
+ */
+export enum EaseOfBusinessEnum {
+  "Muy_importante_(Quiero_emprender_o_invertir)" = "Muy importante (Quiero emprender o invertir).",
+  "Moderadamente_importante_(Podría_considerar_oportunidades)" = "Moderadamente importante (Podría considerar oportunidades).",
+  No_es_importante_para_mí = "No es importante para mí.",
+}
+/**
+ * * `Sí, quiero facilidades para emprender.` - Si
+ * `No me importa.` - No
+ */
+export enum CompanyRegistrationEnum {
+  "Sí,_quiero_facilidades_para_emprender" = "Sí, quiero facilidades para emprender.",
+  No_me_importa = "No me importa.",
+}
+/**
+ * * `Muy importante, quiero vivir en un país con baja contaminación.` - Muy Importante
+ * `No me importa mucho.` - No Importa
+ */
+export enum AirPollutionEnum {
+  "Muy_importante,_quiero_vivir_en_un_país_con_baja_contaminación" = "Muy importante, quiero vivir en un país con baja contaminación.",
+  No_me_importa_mucho = "No me importa mucho.",
+}
+/**
+ * * `Sí, prefiero un país con oportunidades en agricultura.` - Si
+ * `No, prefiero un país con una economía diversificada.` - No
+ * `No me importa.` - No Importa
+ */
+export enum AgricultureEconomyEnum {
+  "Sí,_prefiero_un_país_con_oportunidades_en_agricultura" = "Sí, prefiero un país con oportunidades en agricultura.",
+  "No,_prefiero_un_país_con_una_economía_diversificada" = "No, prefiero un país con una economía diversificada.",
+  No_me_importa = "No me importa.",
+}
+export interface PatchedForm {
+  readonly id?: number;
+  /**
+   * @minimum -9223372036854776000
+   * @maximum 9223372036854776000
+   */
+  age?: number;
+  gender?: GenderEnum;
+  salary?: number;
+  agriculture_economy?: AgricultureEconomyEnum;
+  ease_of_business?: EaseOfBusinessEnum;
+  foreign_investment?: MobileAccessEnum;
+  export_economy?: ExportEconomyEnum;
+  work_sector?: WorkSectorEnum;
+  unemployment_rate?: UnemploymentRateEnum;
+  job_security?: JobSecurityEnum;
+  electricity_access?: MobileAccessEnum;
+  air_pollution?: AirPollutionEnum;
+  living_area?: LivingAreaEnum;
+  healthcare_importance?: HealthcareImportanceEnum;
+  mobile_access?: MobileAccessEnum;
+  research_development?: ResearchDevelopmentEnum;
+  low_taxes?: LowTaxesEnum;
+  company_registration?: CompanyRegistrationEnum;
+  poverty_reduction?: PovertyReductionEnum;
+  unemployment_support?: UnemploymentSupportEnum;
+  readonly created_at?: string;
+  readonly updated_at?: string;
+  user?: number;
+}
+
+export interface Form {
+  readonly id: number;
+  /**
+   * @minimum -9223372036854776000
+   * @maximum 9223372036854776000
+   */
+  age: number;
+  gender?: GenderEnum;
+  salary: number;
+  agriculture_economy?: AgricultureEconomyEnum;
+  ease_of_business?: EaseOfBusinessEnum;
+  foreign_investment?: MobileAccessEnum;
+  export_economy?: ExportEconomyEnum;
+  work_sector?: WorkSectorEnum;
+  unemployment_rate?: UnemploymentRateEnum;
+  job_security?: JobSecurityEnum;
+  electricity_access?: MobileAccessEnum;
+  air_pollution?: AirPollutionEnum;
+  living_area?: LivingAreaEnum;
+  healthcare_importance?: HealthcareImportanceEnum;
+  mobile_access?: MobileAccessEnum;
+  research_development?: ResearchDevelopmentEnum;
+  low_taxes?: LowTaxesEnum;
+  company_registration?: CompanyRegistrationEnum;
+  poverty_reduction?: PovertyReductionEnum;
+  unemployment_support?: UnemploymentSupportEnum;
+  readonly created_at: string;
+  readonly updated_at: string;
+  user: number;
 }
--- app/(logged)/form/page.tsx
+++ app/(logged)/form/page.tsx
@@ -1,76 +1,97 @@
 "use client";
-import useCurrentUser from "@/queries/auth/useCurrentUser";
-import useLogout from "@/queries/auth/useLogout";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import Spinner from "@/components/common/spinner";
-import Link from "next/link";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import useCurrentUser from "@/queries/auth/useCurrentUser";
+import useLogout from "@/queries/auth/useLogout";
+import { useFormsCreate } from "@/api/forms/forms";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Link from "next/link";
+import {
+  GenderEnum,
+  AgricultureEconomyEnum,
+  EaseOfBusinessEnum,
+  ExportEconomyEnum,
+  WorkSectorEnum,
+  UnemploymentRateEnum,
+  JobSecurityEnum,
+  AirPollutionEnum,
+  LivingAreaEnum,
+  HealthcareImportanceEnum,
+  MobileAccessEnum,
+  ResearchDevelopmentEnum,
+  LowTaxesEnum,
+  CompanyRegistrationEnum,
+  PovertyReductionEnum,
+  UnemploymentSupportEnum,
+} from "@/api/baseAppBackendAPI.schemas";
 
 const Form = () => {
   const { user } = useCurrentUser();
   const { logout } = useLogout();
+  const { mutate: createForm } = useFormsCreate();
+  //createForm({
+  //data: {
+  // salary: 0,
+  //age: 0,
+  //user: 0,
+  //weather: WeatherEnum.frio,
+  //work: WorkEnum.agricultor,
+  //gender: GenderEnum.hombre,
+  //country: CountryEnum.albania,
+  //},
+  //});
   return (
     <div
       className="h-screen bg-cover bg-center"
       style={{ backgroundImage: `url('/wallpaper2.jpg')` }}
     >
       <div className="flex items-center justify-center h-full">
-        <Card className="flex flex-col items-center justify-center text-center p-10 w-[600px] gap-4 bg-white bg-opacity-90 shadow-lg rounded-lg">
-          <h1 className="text-4xl font-bold text-gray-800">
+        {/* Ajuste en el Card */}
+        <Card className="flex flex-col items-center p-4 w-[600px] bg-white bg-opacity-90 shadow-lg rounded-lg max-h-[80vh] overflow-y-auto">
+          <h1 className="text-4xl font-bold text-gray-800 mb-6">
             Encontremos tu nuevo hogar
           </h1>
 
-          <style jsx>{`
-            .form-field {
-              width: 100%;
-              padding: 10px;
-              border: 1px solid #ccc;
-              border-radius: 5px;
-              font-size: 16px;
-              background-color: white;
-            }
-          `}</style>
-
+          {/* Formulario con campos alineados a la izquierda */}
           <div className="grid gap-4 w-full">
             <div className="grid gap-2">
-              <Label htmlFor="age">¿Cual es tu edad?</Label>
+              <Label htmlFor="age" className="text-left">¿Cuál es tu edad?</Label>
               <Input
                 id="age"
                 type="number"
                 name="age"
                 min="0"
-                className="form-field"
-                onInput={(e) => {
+                placeholder=""
+                onChange={(e) => {
                   e.target.value = e.target.value.replace(/\D/, "");
                 }}
               />
             </div>
 
             <div className="grid gap-2">
-              <Label htmlFor="gender">¿Cuál es tu sexo?</Label>
-              <select id="gender" name="gender" className="form-field">
-                <option value="hombre">Hombre</option>
-                <option value="mujer">Mujer</option>
-              </select>
+              <Label htmlFor="gender" className="text-left">¿Cuál es tu sexo?</Label>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="male">Hombre</SelectItem>
+                  <SelectItem value="female">Mujer</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
 
             <div className="grid gap-2">
-              <Label htmlFor="work">¿Cual es tu profesión?</Label>
-              <select id="work" name="work" className="form-field">
-                <option value="ingeniero">Ingeniero</option>
-                <option value="agricultor">Agricultor</option>
-                <option value="profesor">Profesor</option>
-                <option value="economista">Economista</option>
-                <option value="deportista">Deportista</option>
-                <option value="artista">Artista</option>
-              </select>
-            </div>
-
-            <div className="grid gap-2">
-              <Label htmlFor="salary">
-                ¿Cual es tu salario actual? (anual)
+              <Label htmlFor="salary" className="text-left">
+                ¿Cuál es tu ingreso mensual aproximado en dólares?
               </Label>
               <Input
                 id="salary"
@@ -78,85 +99,211 @@ const Form = () => {
                 name="salary"
                 step="0.01"
                 min="0"
-                className="form-field"
+                placeholder=""
               />
             </div>
 
             <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="country">¿Pais donde vives actualmente?</Label>
-              </div>
-              <select
-                id="country"
-                name="country"
-                className="border border-gray-300 p-2 rounded"
-                //onChange={handleChange}
-                //value={registerForm.country}
-              >
-                <option value="albania">Albania</option>
-                <option value="andorra">Andorra</option>
-                <option value="armenia">Armenia</option>
-                <option value="austria">Austria</option>
-                <option value="azerbaiyan">Azerbaiyan</option>
-                <option value="bielorrusia">Bielorrusia</option>
-                <option value="belgica">Belgica</option>
-                <option value="bosnia_y_herzegovina">
-                  Bosnia y Herzegovina
-                </option>
-                <option value="bulgaria">Bulgaria</option>
-                <option value="croacia">Croacia</option>
-                <option value="chipre">Chipre</option>
-                <option value="republica_checa">Republica Checa</option>
-                <option value="dinamarca">Dinamarca</option>
-                <option value="estonia">Estonia</option>
-                <option value="finlandia">Finlandia</option>
-                <option value="francia">Francia</option>
-                <option value="georgia">Georgia</option>
-                <option value="alemania">Alemania</option>
-                <option value="grecia">Grecia</option>
-                <option value="hungria">Hungria</option>
-                <option value="islandia">Islandia</option>
-                <option value="irlanda">Irlanda</option>
-                <option value="italia">Italia</option>
-                <option value="kazajistan">Kazajistan</option>
-                <option value="kosovo">Kosovo</option>
-                <option value="letonia">Letonia</option>
-                <option value="liechtenstein">Liechtenstein</option>
-                <option value="lituania">Lituania</option>
-                <option value="luxemburgo">Luxemburgo</option>
-                <option value="malta">Malta</option>
-                <option value="moldavia">Moldavia</option>
-                <option value="monaco">Monaco</option>
-                <option value="montenegro">Montenegro</option>
-                <option value="paises_bajos">Paises Bajos</option>
-                <option value="macedonia_del_norte">Macedonia del Norte</option>
-                <option value="noruega">Noruega</option>
-                <option value="polonia">Polonia</option>
-                <option value="portugal">Portugal</option>
-                <option value="rumania">Rumania</option>
-                <option value="rusia">Rusia</option>
-                <option value="san_marino">San Marino</option>
-                <option value="serbia">Serbia</option>
-                <option value="eslovaquia">Eslovaquia</option>
-                <option value="eslovenia">Eslovenia</option>
-                <option value="espana">España</option>
-                <option value="suecia">Suecia</option>
-                <option value="suiza">Suiza</option>
-                <option value="turquia">Turquia</option>
-                <option value="ucrania">Ucrania</option>
-                <option value="reino_unido">Reino Unido</option>
-                <option value="ciudad_del_vaticano">Ciudad del Vaticano</option>
-              </select>
+              <Label htmlFor="farming" className="text-left">
+                ¿Te interesa un país con una economía agrícola fuerte?
+              </Label>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="farming">
+                    Sí, prefiero un país con oportunidades en agricultura.
+                  </SelectItem>
+                  <SelectItem value="noFarming">
+                    No, prefiero un país con una economía diversificada.
+                  </SelectItem>
+                  <SelectItem value="dontCareFarming">No me importa.</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
 
-            <div className="grid gap-2">
-              <Label htmlFor="weather">Tipo de clima deseado</Label>
-              <select id="weather" name="weather" className="form-field">
-                <option value="caliente">Caliente</option>
-                <option value="medio">Medio</option>
-                <option value="frio">Frío</option>
-              </select>
-            </div>
+            <Label htmlFor="business">¿Qué tan importante es la facilidad para hacer negocios en el país?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="moreBusiness">Muy importante (Quiero emprender o invertir).</SelectItem>
+                <SelectItem value="mediumBusiness">Moderadamente importante (Podría considerar oportunidades).</SelectItem>
+                <SelectItem value="noBusiness">No es importante para mí.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="internationalBusiness">¿Qué tan importante es la inversión extranjera en el país?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="moreInternationalBusiness">Muy importante.</SelectItem>
+                <SelectItem value="noInternationalBusiness">No me importa.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="export">¿Prefieres un país con una economía basada en la exportación?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="moreExport">Sí, quiero un país con un alto nivel de comercio internacional.</SelectItem>
+                <SelectItem value="noExport">No es relevante para mí.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="work">¿En qué sector trabajas o te gustaría trabajar?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="farming">Agricultura / Pesca</SelectItem>
+                <SelectItem value="tech">Tecnología / Innovación</SelectItem>
+                <SelectItem value="businessWork">Negocios / Emprendimiento</SelectItem>
+                <SelectItem value="education">Educación / Salud</SelectItem>
+                <SelectItem value="manufacturing">Industria / Manufactura</SelectItem>
+                <SelectItem value="othersWork">Otros</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="tasa">¿Qué tan importante es la tasa de desempleo para ti?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="lowTasa">Quiero un país con muy baja tasa de desempleo.</SelectItem>
+                <SelectItem value="highTasa">No me importa mucho la tasa de desempleo.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="securityWork">¿Qué nivel de seguridad laboral prefieres?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="goodContracts">Prefiero un país con empleos estables y buenos contratos.</SelectItem>
+                <SelectItem value="noFormal">No me importa si el empleo es informal o vulnerable.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="electricity">¿Qué tan importante es el acceso a la electricidad en zonas urbanas?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="yesElectricity">Muy importante.</SelectItem>
+                <SelectItem value="noElectricity">No me importa.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="contamination">¿Qué tan importante es la contaminación del aire en tu decisión?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="lowContamination">Muy importante, quiero vivir en un país con baja contaminación.</SelectItem>
+                <SelectItem value="highContamination">No me importa mucho.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="rural">¿Quieres vivir en una ciudad o en un área rural?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="cityLife">Ciudad.</SelectItem>
+                <SelectItem value="ruralLife">Rural.</SelectItem>
+                <SelectItem value="noneLife">No me importa.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="helth">¿Qué tan importante es el sistema de salud?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="muchHealth">Muy importante, quiero un país con buena atención médica.</SelectItem>
+                <SelectItem value="noHealth">No me importa mucho.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="phone">¿Qué tan importante es el acceso a la telefonía móvil y la tecnología?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="yesPhone">Muy importante.</SelectItem>
+                <SelectItem value="noPhone">No me importa.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="investigation">¿Prefieres un país con alta inversión en investigación y desarrollo?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="yesInvestigation">Sí, prefiero un país innovador.</SelectItem>
+                <SelectItem value="noInvestigation">No es una prioridad para mí.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="tax">¿Prefieres un país con impuestos más bajos?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="yesTax">Sí, quiero un país con menor carga impositiva.</SelectItem>
+                <SelectItem value="noTax">No me importa.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="newBusiness">¿Quieres un país con facilidad para registrar empresas?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="yesNewBusiness">Sí, quiero facilidades para emprender.</SelectItem>
+                <SelectItem value="noNewBusiness">No me importa.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="poberty">¿Qué tan importante es la reducción de la pobreza en el país?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="yesPoberty">Muy importante, quiero un país con menos desigualdad.</SelectItem>
+                <SelectItem value="noPoberty">No es una prioridad.</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Label htmlFor="programs">¿Te gustaría que el país tenga programas de apoyo al desempleo?</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="yesPrograms">Sí, quiero un país con apoyo social.</SelectItem>
+                <SelectItem value="noPrograms">No me importa.</SelectItem>
+              </SelectContent>
+            </Select>
 
             <Link href="/home">
               <Button className="bg-blue-600 text-white px-6 py-3 mt-4 w-full">
@@ -171,3 +318,5 @@ const Form = () => {
 };
 
 export default Form;
+
+
